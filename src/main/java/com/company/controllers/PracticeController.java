ff8b79d299f969bd4a2d04faa32b88c50dc516a7
Mitra <mitramoghadasi@gmail.com>
2025-02-08 15:47:20 +0100

    Add Controller

--- src/main/java/com/company/controllers/PracticeController.java
+++ src/main/java/com/company/controllers/PracticeController.java
@@ -1,4 +1,26 @@
 package com.company.controllers;
 
+import com.company.models.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class PracticeController {
+
+    @GetMapping("/a1")
+    public String hello(){
+        return "Hello";
+    }
+
+    @GetMapping("/a2")
+    public String hello2(){
+        return "Hello2";
+    }
+    @GetMapping("/p1")
+    public Product getProduct(){
+        Product cream = new Product("Nivea", "Face", 1);
+        return cream;
+    }
 }
---
+++ src/main/java/com/company/models/Product.java
@@ -0,0 +1,4 @@
+package com.company.models;
+
+public record Product(String name, String category, Integer id) {
+}
