147cfecf4a0e8da0d1de3fbec0b3d03d91262c00
Mitra <mitramoghadasi@gmail.com>
2025-02-08 13:33:50 +0100

    Add Spring Boot

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
-        School school = new School();
-
-        System.out.println("Hello World");
+        SpringApplication.run(Main.class, args);
     }
+
 }
